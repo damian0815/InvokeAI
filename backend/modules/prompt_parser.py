5383f691874a58ab01cda1e4fac6cf330146526a
Damian at mba <damian@frey.NOSPAMco.nz>
2022-10-10 02:27:47 +0200

    prompt parser seems to work

---
+++ backend/modules/prompt_parser.py
@@ -0,0 +1,105 @@
+import pyparsing
+import pyparsing as pp
+from pyparsing import original_text_for
+
+
+class CFGScale():
+    def __init__(self, scale_factor: float, fragment: str):
+        self.fragment = fragment
+        self.scale_factor = scale_factor
+        #print(f"S: requested CFGScale '{fragment}' x {scale_factor}")
+
+    def __repr__(self):
+        return f"CFGScale('{self.fragment}' x {self.scale_factor})"
+    def __eq__(self, other):
+        return type(other) is CFGScale and other.scale_factor == self.scale_factor and other.fragment == self.fragment
+
+
+class Attention():
+    def __init__(self, weight: float, fragment: str):
+        self.weight = weight
+        self.fragment = fragment
+        #print(f"A: requested attention '{fragment}' to {weight}")
+
+    def __repr__(self):
+        return f"Attention('{self.fragment}' @ {self.weight})"
+    def __eq__(self, other):
+        return type(other) is Attention and other.weight == self.weight and other.fragment == self.fragment
+
+class Word():
+    def __init__(self, text: str):
+        self.text = text
+
+    def __repr__(self):
+        return "Word('"+self.text+"')"
+    def __eq__(self, other):
+        return type(other) is Word and other.text == self.text
+
+class Blend():
+    def __init__(self, prompts: list, weights: list[float]):
+        print("making Blend with prompts", prompts, "and weights", weights)
+        if len(prompts) != len(weights):
+            raise PromptParser.ParsingException("().blend(): mismatched prompt/weight counts")
+        self.prompts = prompts
+        self.weights = weights
+
+    def __repr__(self):
+        return f"Blend({[self.prompts]} | weights {self.weights}"
+    def __eq__(self, other):
+        return other.__repr__() == self.__repr__()
+
+
+class PromptParser():
+
+    class ParsingException(Exception):
+        pass
+
+    def __init__(self):
+
+        lparen = pp.Suppress("(")
+        rparen = pp.Suppress(")")
+
+        number = pyparsing.pyparsing_common.real | pp.Word(pp.nums).set_parse_action(pp.token_map(float))
+
+        attention_explicit = pp.Group(number + lparen + pp.CharsNotIn(')') + rparen)
+        attention_plus = pp.Group(pp.Word('+') + lparen + pp.CharsNotIn(')') + rparen)
+        attention_minus = pp.Group(pp.Word('-') + lparen + pp.CharsNotIn(')') + rparen)
+        attention_explicit.set_parse_action(lambda x: Attention(weight=float(x[0][0]), fragment=x[0][1]))
+        attention_plus.set_parse_action(lambda x: Attention(weight=pow(1.1, len(x[0][0])), fragment=x[0][1]))
+        attention_minus.set_parse_action(lambda x: Attention(weight=pow(0.9, len(x[0][0])), fragment=x[0][1]))
+
+        # attention: number(fragment)
+        attention = attention_plus | attention_minus | attention_explicit
+
+        word = pp.Word(pp.printables).set_parse_action(lambda x: Word(' '.join([s for s in x])))
+        prompt_part = attention | word
+
+
+        prompt = pp.Optional(pp.OneOrMore(prompt_part))
+
+        # cfg scale: (fragment).scale(number)
+        blend_terms = pp.QuotedString('"') + pp.ZeroOrMore(pp.Suppress(",") + pp.QuotedString('"'))
+        blend_weights = number + pp.ZeroOrMore(pp.Suppress(",") + number)
+        blend = pp.Group(lparen + pp.Group(blend_terms) + rparen + pp.Literal(".blend").suppress() + lparen + pp.Group(blend_weights) + rparen)
+
+        self.prompt = blend | prompt
+
+        blend.set_parse_action(lambda x: Blend(prompts=[list(prompt.parseString(p)) for p in x[0][0]], weights=x[0][1]))
+
+
+    def parse(self, prompt):
+        '''
+        :param prompt: The prompt string to parse
+        :return: a tuple
+        '''
+        #print("parsing", prompt)
+
+        parts = self.prompt.parseString(prompt)
+        return parts
+
+def parse_prompt(prompt_string):
+    pp = PromptParser()
+    print(f"parsing '{prompt_string}'")
+    parsed = pp.parse(prompt_string)
+    #print(f"-> parsed '{prompt_string}' to ", parsed)
+    return list(parsed)
---
+++ tests/test_prompt_parser.py
@@ -0,0 +1,39 @@
+import unittest
+
+from backend.modules.prompt_parser import parse_prompt, Word, Attention, Blend
+
+
+empty_conditioning = [('', 1)]
+
+class PromptParserTestCase(unittest.TestCase):
+
+    def test_empty(self):
+        self.assertEqual(parse_prompt(''), [])
+
+    def test_basic(self):
+        self.assertEqual(parse_prompt("fire (flames)"), [Word("fire"), Word("(flames)")])
+        self.assertEqual(parse_prompt("fire flames"), [Word("fire"), Word("flames")])
+        self.assertEqual(parse_prompt("fire, flames"), [Word("fire,"), Word("flames")])
+        self.assertEqual(parse_prompt("fire, flames , fire"), [Word("fire,"), Word("flames"), Word(","), Word("fire")])
+
+    def test_attention(self):
+        self.assertEqual(parse_prompt("fire 0.5(flames)"), [Word("fire"), Attention(0.5, "flames")])
+        self.assertEqual(parse_prompt("0.5(flames)"), [Attention(0.5, "flames")])
+        self.assertEqual(parse_prompt("+(flames)"), [Attention(1.1, "flames")])
+        self.assertEqual(parse_prompt("-(flames)"), [Attention(0.9, "flames")])
+        self.assertEqual(parse_prompt("++(flames)"), [Attention(pow(1.1,2), "flames")])
+        self.assertEqual(parse_prompt("--(flames)"), [Attention(pow(0.9,2), "flames")])
+
+        #self.assertEqual(pp.parse(prompt))
+        #self.assertEqual(True, False)  # add assertion here
+
+    def test_blend(self):
+        self.assertEqual(parse_prompt("(\"fire\", \"fire flames\", \"hi\").blend(0.7, 0.3, 1.0)"),
+                         [Blend([[Word("fire")], [Word("fire"), Word("flames")], [Word("hi")]], [0.7, 0.3, 1.0])])
+
+    def test_nested(self):
+        self.assertEqual(parse_prompt('("fire ++(flames)", "mountain 2(man)").blend(1,1)'),
+                        [Blend([[Word("fire"), Attention(pow(1.1,2), "flames")], [Word("mountain"), Attention(2.0, "man")]], [1.0,1.0])])
+
+if __name__ == '__main__':
+    unittest.main()
