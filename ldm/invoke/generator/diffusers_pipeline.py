9e65256dee5f45abdd4786a2552b40011e5c6c66
Damian Stewart <d@damianstewart.com>
2022-12-14 20:31:17 +0100

    inpainting and outpainting working with attention maps

--- ldm/invoke/generator/diffusers_pipeline.py
+++ ldm/invoke/generator/diffusers_pipeline.py
@@ -533,7 +533,7 @@ class StableDiffusionGeneratorPipeline(StableDiffusionPipeline):
 
         with torch.inference_mode():
             image = self.decode_latents(result_latents)
-            output = InvokeAIStableDiffusionPipelineOutput(images=image, nsfw_content_detected=[], attention_maps_image=result_attention_maps)
+            output = InvokeAIStableDiffusionPipelineOutput(images=image, nsfw_content_detected=[], attention_map_saver=result_attention_maps)
             return self.check_for_safety(output, dtype=text_embeddings.dtype)
 
     def non_noised_latents_from_image(self, init_image, *, device, dtype):
