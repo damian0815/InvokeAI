56d82aadffe7bf4da4fd1560d604aa8e908f252b
Damian <d@damianstewart.com>
2025-12-04 12:49:47 +0100

    add flow_prediction scheduler type

--- invokeai/backend/model_manager/legacy_probe.py
+++ invokeai/backend/model_manager/legacy_probe.py
@@ -869,7 +869,7 @@ class PipelineFolderProbe(FolderProbeBase):
     def get_scheduler_prediction_type(self) -> SchedulerPredictionType:
         with open(self.model_path / "scheduler" / "scheduler_config.json", "r") as file:
             scheduler_conf = json.load(file)
-        if scheduler_conf.get("prediction_type", "epsilon") in ["flow-matching", "v_prediction"]:
+        if scheduler_conf.get("prediction_type", "epsilon") in ["flow-matching", "flow_prediction", "v_prediction"]:
             return SchedulerPredictionType.VPrediction
         elif scheduler_conf.get("prediction_type", "epsilon") == "epsilon":
             return SchedulerPredictionType.Epsilon
